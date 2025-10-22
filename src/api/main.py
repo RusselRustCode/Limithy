70a727a939159ec3cb2a6e89fb5b4e9d25d7af79
RusselDRust <noobboy0307@gmail.com>
2025-10-22 18:45:39 +0500

    Добавил по мелочи

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from config.settings import settings
+from core.database import connect_to_mongo, close_mongo_db, get_database
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
 
-app = FastAPI()
+@app.on_event("startup")
+async def startup_event():
+    print("")
+    connect_to_mongo()
 
-@app.get("/")
-def root():
-    return "1"
+@app.on_event("shutdown")
+async def shutdown_event():
+    print(" ")
+    close_mongo_db()
+
+
+app.include_router(
+    trace.router,
+    prefix=settings.API_V1_STR,
+    tags=["TRACE"]
+)
--- src/core/database.py
+++ src/core/database.py
@@ -9,20 +9,19 @@ class DataBase:
     db: Optional[AsyncIOMotorDatabase] = None
 
 
-    def connect_to_mongo():
-        print(f"{settings.MONGODB_URL}")
-        DataBase.client = AsyncIOMotorClient(settings.MONGODB_URL)
-        DataBase.db = AsyncIOMotorDatabase(settings.MONGODB_NAME)
-        print(f"Подключено к {settings.MONGODB_NAME}")
+def connect_to_mongo():
+    print(f"{settings.MONGODB_URL}")
+    DataBase.client = AsyncIOMotorClient(settings.MONGODB_URL)
+    DataBase.db = AsyncIOMotorDatabase(settings.MONGODB_NAME)
+    print(f"Подключено к {settings.MONGODB_NAME}")
 
-    def close_mongo_db():
+def close_mongo_db():
+    if DataBase.client:
+        DataBase.client.close()
+        print("Соединение закрыто")
 
-        if DataBase.client:
-            DataBase.client.close()
-            print("Соединение закрыто")
-
-    def get_database() -> AsyncIOMotorDatabase:
-        if DataBase.db is None:
-            raise ConnectionError("БД не инициализированна")
-        return DataBase.db
+def get_database() -> AsyncIOMotorDatabase:
+    if DataBase.db is None:
+        raise ConnectionError("БД не инициализированна")
+    return DataBase.db
     
